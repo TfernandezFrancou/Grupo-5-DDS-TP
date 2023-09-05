cddf07d2e664e48ecf8f131e95542ee3c2a98e3f
JuanMoscatelli1 <jmoscatelli@frba.utn.edu.ar>
2023-09-05 17:08:36 -0300

    pom y persistence.xml

---
+++ src/test/java/domain/ContextTest.java
@@ -0,0 +1,39 @@
+package domain;
+
+import org.junit.Test;
+import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
+import org.uqbarproject.jpa.java8.extras.test.AbstractPersistenceTest;
+import utils.BDUtils;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.Assert.assertNotNull;
+
+public class ContextTest extends AbstractPersistenceTest implements WithGlobalEntityManager {
+    @Test
+    public void contextUp() {
+        assertNotNull(entityManager());
+    }
+
+    @Test
+    public void contextUpWithTransaction() {
+        withTransaction(() -> {});
+    }
+
+    /*
+
+    @Test
+    public void init(){
+        EntityManager em = BDUtils.getEntityManager();
+        BDUtils.comenzarTransaccion(em);
+
+        Corredor corredor1 = new Corredor();
+        corredor1.setNombre("Jorge");
+
+        em.persist(corredor1);
+
+        BDUtils.commit(em);
+    }
+    */
+}
+

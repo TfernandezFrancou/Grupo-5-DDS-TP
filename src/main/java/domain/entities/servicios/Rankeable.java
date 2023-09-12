d4bba4a2db0031c659b8048cca778ae1564645f7
Maxi <mafiorentino@frba.utn.edu.ar>
2023-09-11 23:40:50 -0300

    cambio rankeable a joined

--- src/main/java/domain/entities/servicios/Rankeable.java
+++ src/main/java/domain/entities/servicios/Rankeable.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 
 @Getter
 @Entity
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Inheritance(strategy = InheritanceType.JOINED)
 public abstract class Rankeable {
     @Column
     private String nombre;

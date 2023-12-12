d22070f5d9d5c949b894b85313d4bd843cb17976
JuanMoscatelli1 <jmoscatelli@frba.utn.edu.ar>
2023-12-12 16:18:27 -0300

    Funcionalidad de administrar tipos de usuario en Pesado

--- src/main/java/handlers/PatchTipoDeMiembroHandler.java
+++ src/main/java/handlers/PatchTipoDeMiembroHandler.java
@@ -1,10 +1,10 @@
 package handlers;
 
-import domain.entities.actores.miembros.Miembro;
 import domain.entities.actores.miembros.MiembroPorComunidad;
 import domain.entities.actores.miembros.TipoDeMiembro;
 import domain.entities.repositorios.ComunidadesRepo;
 import domain.entities.repositorios.RepoMiembros;
+import dto.PatchTipoMiembroResponse;
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
 import org.jetbrains.annotations.NotNull;
@@ -15,29 +15,32 @@ import javax.persistence.EntityManager;
 public class PatchTipoDeMiembroHandler implements Handler {
     @Override
     public void handle(@NotNull Context context) throws Exception {
-        String tipoDeMiembro=context.formParamAsClass("tipoMiembro",String.class).get();
-        int idComunidad=context.formParamAsClass("idComunidad", int.class).get();
-        String id_sesion=context.cookie("id_sesion");
-        Miembro miembro= SesionManager.get().obtenerMiembro(id_sesion);
-        System.out.println("Se manda el form");
-        System.out.println(tipoDeMiembro);
+        try {
+            System.out.println("Entro al handler pesado");
 
-        MiembroPorComunidad miembroPorComunidad = ComunidadesRepo.getInstance().obtenerMiembroPorComunidad(miembro.getMiembro_codigo(),idComunidad);
-        EntityManager em = utils.BDUtils.getEntityManager();
-        BDUtils.comenzarTransaccion(em);
-        MiembroPorComunidad miembroACambiar = em.find(MiembroPorComunidad.class,miembroPorComunidad.getMiembroPorComunidad_codigo());
 
-        if(tipoDeMiembro.equalsIgnoreCase("Observador")){
-            TipoDeMiembro nuevoTipo= RepoMiembros.getInstance().buscarTipoDeMiembro("Observador");
-            miembroACambiar.setTipoDeMiembro(nuevoTipo);
+            PatchTipoMiembroResponse info = context.bodyAsClass(PatchTipoMiembroResponse.class);
+
+            MiembroPorComunidad miembroPorComunidad = ComunidadesRepo.getInstance().obtenerMiembroPorComunidad(info.getMiembroId(),Integer.parseInt(info.getComunidadId()));
+            EntityManager em = BDUtils.getEntityManager();
+            BDUtils.comenzarTransaccion(em);
+            MiembroPorComunidad miembroACambiar = em.find(MiembroPorComunidad.class, miembroPorComunidad.getMiembroPorComunidad_codigo());
+
+            TipoDeMiembro tipoACambiar;
+            if (info.getNuevoTipo().equalsIgnoreCase("Observador")) {
+                tipoACambiar = RepoMiembros.getInstance().buscarTipoDeMiembro("Observador");
+            } else {
+                tipoACambiar = RepoMiembros.getInstance().buscarTipoDeMiembro("Usuario de Servicio");
+            }
+
+            miembroACambiar.setTipoDeMiembro(tipoACambiar);
+            BDUtils.commit(em);
+            System.out.println("Tipo encontrado: " + tipoACambiar.getTipoDeMiembro());
+
+            context.status(200).result("Tipo de miembro actualizado correctamente.");
+        } catch (Exception e) {
+            e.printStackTrace();
+            context.status(500).result("Error interno del servidor al actualizar el tipo de miembro.");
         }
-            else{
-            TipoDeMiembro nuevoTipo=RepoMiembros.getInstance().buscarTipoDeMiembro("Usuario de Servicio");
-            miembroACambiar.setTipoDeMiembro(nuevoTipo);
-        }
-        BDUtils.commit(em);
-
-        context.redirect("/perfil/");
-
     }
 }

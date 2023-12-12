8184163b9394d419ebf10b766d4c417820330e9a
JuanFrancisco9 <jucaceres@frba.utn.edu.ar>
2023-12-12 11:44:30 -0300

    Listo el json para armar la pagina de perfil comunidad en Pesado

--- src/main/java/handlers/GetPerfilComunidadHandler.java
+++ src/main/java/handlers/GetPerfilComunidadHandler.java
@@ -1,5 +1,10 @@
 package handlers;
 
+import domain.entities.actores.miembros.Miembro;
+import domain.entities.actores.miembros.MiembroPorComunidad;
+import domain.entities.repositorios.ComunidadesRepo;
+import domain.entities.repositorios.RepoMiembros;
+import dto.ComunidadPresentacion;
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
 import org.jetbrains.annotations.NotNull;
@@ -8,6 +13,12 @@ public class GetPerfilComunidadHandler implements Handler {
     @Override
     public void handle(@NotNull Context context) throws Exception {
         String idSesion=context.cookie("id_sesion");
+        Integer idComunidad = context.pathParamAsClass("idComunidad", Integer.class).get();
+        Miembro miembro = SesionManager.get().obtenerMiembro(idSesion);
+        MiembroPorComunidad miembroPorComunidad = ComunidadesRepo.getInstance().obtenerMiembroPorComunidad(miembro.getMiembro_codigo(), idComunidad);
 
+        ComunidadPresentacion comunidad = new ComunidadPresentacion(miembroPorComunidad.getComunidad(),miembroPorComunidad);
+
+        context.json(comunidad);
     }
 }
